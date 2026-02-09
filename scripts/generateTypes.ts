542e64a9eb8618f845f0044dee94cfff917f3c78
signal32 <hdweir@outlook.com>
2026-02-09 13:39:56 +0000

    Rename schema to shop
    Yes I will also change other references from store to shop

--- scripts/generateTypes.ts
+++ scripts/generateTypes.ts
@@ -3,4 +3,4 @@ import { execSync } from "child_process"
 process.loadEnvFile()
 
 const dbUrl = `postgresql://${process.env['POSTGRES_USER']}:${process.env['POSTGRES_PASSWORD']}@localhost:5432/store`
-execSync(`npx supabase gen types typescript --db-url ${dbUrl} --schema api > src/database.ts`)
+execSync(`npx supabase gen types typescript --db-url ${dbUrl} --schema shop > src/database.ts`)
--- src/product.ts
+++ src/product.ts
@@ -1,3 +1,3 @@
 import { type Database } from './database/types.ts'
 
-export type Product = Database['api']['Tables']['todos']['Row']
+export type Product = Database['shop']['Tables']['todos']['Row']
