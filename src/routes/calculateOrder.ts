0e954d2dc15c0bca2ced4de3a1fc7908e407d178
signal32 <hdweir@outlook.com>
2026-06-08 11:10:24 +0100

    Handle available products in order

--- src/routes/calculateOrder.ts
+++ src/routes/calculateOrder.ts
@@ -1,5 +1,8 @@
+import { pool } from "#src/database/client.ts"
 import type { PostHandler } from "#src/handler.ts"
 import { isOrder, iterOrderProducts, type Order } from "#src/order.ts"
+import { isProduct } from "#src/product.ts"
+import { findProductById } from "#src/queries/queries.queries.ts"
 import { getProductPrice } from "./productPrice.ts"
 
 export const calculateOrder: PostHandler<
@@ -16,12 +19,16 @@ export const calculateOrder: PostHandler<
 
 
 export async function calculateOrderTotals(order: Order) {
-    const linePrices = await Promise.all(iterOrderProducts(order).map(async ({ product, config, optionId }) => {
+    const linePrices = await Promise.all(iterOrderProducts(order).map(async ({ product: { id }, config, optionId }) => {
+        const [product] = await findProductById.run({ productId: id }, pool)
+        if (!isProduct(product)) throw new Error('not a product')
+
         const quantity = config.quantity
-        const unitPrice = await getProductPrice(product, config.options)
+        const productPrice = await getProductPrice(product, config.options)
+        const unitPrice = productPrice.available ? productPrice.price : NaN
         const linePrice = unitPrice * quantity
 
-        return { unitPrice, linePrice, productId: product.id, optionId }
+        return { unitPrice, linePrice, productId: product.id, optionId, invalid: !product.available }
     }))
 
     const totalPrice = linePrices.reduce((total, { linePrice }) => total + linePrice, 0)
